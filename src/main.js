84a834b197f5db034d57d2f6638cda40095c4e7e
Prince4ademola <149257657+Prince4ademola@users.noreply.github.com>
2023-11-16 13:51:41 +0100

    Add calculator project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,83 @@
-// Add your JavaScript here
+let currentInput = '';
+let currentOperation = null;
+let previousInput = '';
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+    currentInput = '';
+    currentOperation = null;
+    previousInput = '';
+}
+
+function deleteLastCharacter() {
+    currentInput = currentInput.slice(0, -1);
+    updateDisplay();
+}
+
+function appendNumber(number) {
+    currentInput += number;
+    updateDisplay();
+}
+
+function appendDecimal() {
+    if (!currentInput.includes('.')) {
+        currentInput += '.';
+        updateDisplay();
+    }
+}
+
+function calculatePercentage() {
+    if (currentInput !== '') {
+        const percentageValue = parseFloat(currentInput) / 100;
+        currentInput = percentageValue.toString();
+        updateDisplay();
+    }
+}
+
+function setOperation(operation) {
+    if (currentInput !== '') {
+        if (operation === '%') {
+            calculatePercentage();
+        } else {
+            calculateResult();
+            previousInput = currentInput;
+            currentInput = '';
+            currentOperation = operation;
+        }
+    }
+}
+
+function calculateResult() {
+    if (previousInput !== '' && currentInput !== '') {
+        let result;
+        const num1 = parseFloat(previousInput);
+        const num2 = parseFloat(currentInput);
+
+        switch (currentOperation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                break;
+        }
+
+        clearDisplay();
+        currentInput = result.toString();
+        updateDisplay();
+        previousInput = '';
+        currentOperation = null;
+    }
+}
+
+function updateDisplay() {
+    document.getElementById('display').value = currentInput;
+}
